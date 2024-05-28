265438b5b0b65307a5557e9119984e6bf6c11632
sravancipher <sravanpotnuru24@gmail.com>
2024-05-28 14:34:50 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,29 +4,36 @@ import axios from 'axios'
 function App() {
   const [mail,setMail]=useState();
   const[password,setPassword]=useState();
-  async function resetpswd(){
-    axios.put( `https://screensagadb.up.railway.app/user/updatepassword/${mail}/${password}`)
-    .then(response => {
-        console.log('Success:', response.data);
-        if(response.data){
+  const[resdata,setResData]=useState("");
+  const[response,setResponse]=useState(false);
+  async function resetpswd(e){
+    e.preventDefault();
+    axios.put( `http://localhost:8081/user/updatepassword/${mail}/${password}`,{
             
-            alert('Password Updated Successfully. Login Again');
-            // setTimeout(()=>{},3000)
-        }
-        else{
-            alert("The email address provided does not correspond to an existing account")
-            
-        }
-    })
-    
-    .catch(error => {
-        console.error("error is occured",error);
-        alert('An error occurred while sending the reset link.');
-        // setTimeout(()=>{},3000)
-    });
+    header:{
+       'Content-Type':'application/json'
     }
-    
-
+ })
+    .then(
+      res=>{
+        if(res.data){
+      
+          setResData("Password Updated Successfully. Login Again")
+          setResponse(true);
+          console.log("update pswd response",res.data);
+          // alert('Password Updated Successfully. Login Again');
+          // setTimeout(()=>{},3000)
+      
+      } else{
+        console.log("update pswd response",res.data);
+        setResData("The email address provided does not correspond to an existing account");
+        setResponse(false);
+          // alert("The email address provided does not correspond to an existing account")
+      }
+      }
+    )
+    .catch(err=>{console.log("update pswd error",err)})
+  }
 
   return (
     <div className="App">
@@ -34,7 +41,7 @@ function App() {
     <div className='col-md-3 col-1'></div>
     <div className='col-md-6 col-10'>
       <h1 className="text-light" style={{textAlign: "center"}}>Reset Password</h1>
-      <form method="post" onSubmit={resetpswd}>
+      <form onSubmit={resetpswd} method="post">
         <div className="form-floating mb-3 mt-3">
           <input type="email" className="form-control" id="email" placeholder="Enter email" name="email" required value={mail} onChange={(e)=>{setMail(e.target.value)}}/>
           <label for="email" className="text-dark">Enter Email</label>
@@ -43,8 +50,9 @@ function App() {
           <input type="password" className="form-control " id="pwd" placeholder="Reset Password" name="pswd" required value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
           <label for="pwd" className="text-dark">Reset Password</label>
         </div>
+        {response?<h5 class="text-success">{resdata}</h5>:<h5 class="text-danger">{resdata}</h5>}
         <div className="d-grid">
-        <button className="btn btn-danger">Reset Password</button>
+        <button type="submit"  className="btn btn-danger">Reset Password</button>
       </div>
       </form>
       </div>
